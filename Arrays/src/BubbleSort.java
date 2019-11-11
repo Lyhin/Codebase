7034ad25e21c37fa463c86c037ec0809d9750a17
Lyhin <40678308+Lyhin@users.noreply.github.com>
2019-11-11 23:35:37 +0900

    Update BubbleSort.java

--- Arrays/src/BubbleSort.java
+++ Arrays/src/BubbleSort.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.Arrays;
+
 /**
  * 冒泡排序(由小到大)
  * 从前往后进行两两比较，如果前面的数大就进行交换，把大的数交换到后面
@@ -14,7 +14,6 @@ public class BubbleSort {
 
         for (int x = 0; x < data.length-1; x++) {
            
-
             for (int i = 0; i < data.length - 1-x; i++) {
                 if (data[i] < data[i + 1]) {
                     int t = data[i];
