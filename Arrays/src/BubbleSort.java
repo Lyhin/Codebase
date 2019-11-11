682e7e0d521b38a5df3b9ca8e73b87f8b7f1565f
Lyhin <linyuanhang@msn.com>
2019-11-11 23:09:18 +0900

    Codebase

--- Arrays/src/BubbleSort.java
+++ Arrays/src/BubbleSort.java
@@ -2,13 +2,8 @@ public class BubbleSort {
     public static void mian(String[] args) {
         int[] date = {21, 34, 65, 8, 90, 12};
         for (int i = 0; i < date.length; i++) {
-            for(int x =0; x<date.length-1; x++){
-                if(date[x+1]>date[x]){
-                    int t = date[x];
-                    date[x]=date[x+1];
-                    date[x+1]=t;
-                }
-            }
+            
+
         }
 
     }
